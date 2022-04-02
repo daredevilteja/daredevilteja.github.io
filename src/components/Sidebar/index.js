93405910f0eae7c8937458aa9e342f217f48de0e
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2022-04-02 22:38:03 +0530

    Added icons and respective styles

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./index.scss";
 import LogoS from "../../assets/images/logo-s.png";
 import LogoSubtitle from "../../assets/images/logo_sub.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Sidebar = () => {
   return (
@@ -10,6 +17,56 @@ const Sidebar = () => {
         <img src={LogoS} alt="logo" />
         <img className="sub-logo" src={LogoSubtitle} alt="logo" />
       </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to={"/"}>
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className={"about-link"}
+          to={"/about"}
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className={"contact-link"}
+          to={"/contact"}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target={"_blank"}
+            rel="noreferrer"
+            href="https://github.com/daredevilteja"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target={"_blank"}
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/raviteja-n/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target={"_blank"}
+            rel="noreferrer"
+            href="https://twitter.com/daredevilteja"
+          >
+            <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
