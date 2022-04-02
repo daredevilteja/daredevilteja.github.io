dd25fbca9c818ebf31f58b7b71d5e4e3f432ebd2
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2022-04-02 22:05:53 +0530

    Added logo and corresponding styles

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
 import "./index.scss";
+import LogoS from "../../assets/images/logo-s.png";
+import LogoSubtitle from "../../assets/images/logo_sub.png";
 
 const Sidebar = () => {
-  return <>Hello from Sidebar</>;
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to={"/"}>
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="logo" />
+      </Link>
+    </div>
+  );
 };
 
 export default Sidebar;
